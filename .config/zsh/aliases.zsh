cdff9e7570a4d01337529fd4ad1b7200ec602dbc
Thanachote Ngamkana <gbb.tn19@gmail.com>
2026-07-18 04:38:06 +0700

    feat(zsh): update home-manager flake target and enhance devcontainer aliases
    - Update `hms` alias to target the `default` flake with `--impure` flag.
    - Convert `dev-exec` alias to a function that supports custom command arguments, defaulting to `zsh`.
    - Update `dev-up` dotfiles repository URL and installation command.

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,11 +1,17 @@
 # Aliases
 alias gq='ghq get -l -p'
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
-alias hms="home-manager switch --flake ~/.config/home-manager#nenz"
+alias hms="home-manager switch --flake ~/.config/home-manager#default --impure"
 alias kps='keepassxc-cli show ~/KeePass/onez-password.kdbx'
 
-alias dev-exec='devcontainer exec --workspace-folder . zsh'
+dev-exec() {
+  if [ $# -eq 0 ]; then
+    devcontainer exec --workspace-folder . zsh
+  else
+    devcontainer exec --workspace-folder . "$@"
+  fi
+}
 alias dev-up='devcontainer up \
   --workspace-folder . \
-  --dotfiles-repository https://github.com/gbb19/devcontainer-dotfile.git \
-  --dotfiles-install-command devcontainer/install-dotfiles'
+  --dotfiles-repository https://github.com/gbb19/gbbyte-dotfiles.git \
+  --dotfiles-install-command setup/setup_devcontainer.sh'
