6a751a269432c644dfa5d344a241ab3e59360438
Thanachote Ngamkana <gbb.tn19@gmail.com>
2026-07-18 16:24:59 +0700

    chore(zsh): alias tmux to force UTF-8 mode

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,6 +3,8 @@ alias gq='ghq get -l -p'
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
 alias hms="home-manager switch --flake ~/.config/home-manager#default --impure"
 alias kps='keepassxc-cli show ~/KeePass/onez-password.kdbx'
+alias tmux='tmux -u'
+
 
 dev-exec() {
   if [ $# -eq 0 ]; then
