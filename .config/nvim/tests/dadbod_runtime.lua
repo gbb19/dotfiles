1a5436967c96fadad622c263ffbd163315a0e4af
Thanachote Ngamkana <gbb.tn19@gmail.com>
2026-07-24 20:28:18 +0700

    test(dadbod): characterize dbout exports

--- .config/nvim/tests/dadbod_runtime.lua
+++ .config/nvim/tests/dadbod_runtime.lua
@@ -1,4 +1,5 @@
 local dadbod = require("plugins.dadbod")
+local cell = require("plugins.dadbod.cell")
 local query = require("plugins.dadbod.query")
 
 local failures = {}
@@ -21,6 +22,44 @@ for _, name in ipairs({
 }) do
   check(type(query[name]) == "function", ("Dadbod query API changed: %s"):format(name))
 end
+
+for _, name in ipairs({
+  "yank_in_clause",
+  "yank_insert_statements",
+  "yank_csv",
+}) do
+  check(type(cell[name]) == "function", ("Dadbod cell export API changed: %s"):format(name))
+end
+
+local dbout_buf = vim.api.nvim_create_buf(false, true)
+vim.api.nvim_buf_set_name(dbout_buf, "/tmp/Result_1_users.dbout")
+vim.api.nvim_set_current_buf(dbout_buf)
+vim.bo[dbout_buf].filetype = "dbout"
+vim.api.nvim_buf_set_lines(dbout_buf, 0, -1, false, {
+  "id | name",
+  "---+-------",
+  "1  | O'Neil",
+})
+vim.b[dbout_buf].original_lines = {
+  "id | name",
+  "---+-------",
+  "1  | O'Neil",
+}
+vim.api.nvim_win_set_cursor(0, { 3, 0 })
+
+cell.yank_in_clause()
+check(vim.fn.getreg('"') == "id IN (1)", "SQL IN-clause generation changed")
+
+cell.yank_insert_statements()
+check(
+  vim.fn.getreg('"') == "INSERT INTO users (id, name) VALUES\n  (1, 'O''Neil');",
+  "SQL INSERT generation changed"
+)
+
+cell.yank_csv()
+check(vim.fn.getreg('"') == "id,name\n1,O'Neil", "dbout CSV generation changed")
+
+vim.api.nvim_buf_delete(dbout_buf, { force = true })
 
 for _, name in ipairs({
   "run_sql_block",
